a2404429e3dda07a6120ffaa53a754a484b61a90
Pedro de Medeiros <pedro.medeiros@gmail.com>
2022-02-10 19:06:58 -0300

    OS agnostic improvements, tileset construction and more

--- README.md
+++ README.md
@@ -1,15 +1,10 @@
 tilegen.py
 ==========
 
-Splits big image into smaller tiles and checks MD5 signature for repeated images and delete them if found. **Requires ImageMagick to work.** Usage is best explained by example:
+Splits big image into smaller tiles, checks MD5 signature for repeated images, and delete them if found. After the purge, a new smaller tileset is created. **Requires ImageMagick to work.** Usage is best explained by example:
 
 ``` 
-./tilegen.py big_image.png 16x16 directory/tile
+./tilegen.py ./sample/maniac.png 16x16 result/tile 256x192
 ```
 
-will create `directory` and, inside it, tiles from `tile0000.png` to `tilennnn.png` where `nnnn` is the number of times a 16x16 tile fits inside `big_image.png`. A list of removed files called `removed.txt` will be created inside `directory` if any file was removed.
-
-TODO
-----
-
-A different script that joins the tiles together into a tileset, but the repeated tiles are removed.
+will create `result` directory and, inside it, tiles from `tile0000.png` to `tilennnn.png` where `nnnn` is the number of times a 16x16 tile fits inside `sample/maniac.png`. Identical tiles are removed if found, so the actual number of tiles may be smaller than that. A list of removed tiles called `removed.txt` will be created inside `result` along with a *Tiled*-compatible `map.json` and a new tileset image called `tileset256x192.png`.
--- tilegen.py
+++ tilegen.py
@@ -2,50 +2,167 @@
 
 import sys
 import os
+import re
+import json
+
 import subprocess
+from subprocess import DEVNULL, STDOUT, check_call
+
+from collections import OrderedDict
+from io import StringIO
+from PIL import Image
+
+
+debug = lambda *x: None
 
 
 def main():
-    if len(sys.argv) != 4:
-        sys.exit("usage: %s big_image_file tile_dimensions tilemap_prefix" % sys.argv[0])
+    if len(sys.argv) != 5:
+        sys.exit(f'usage: {sys.argv[0]} big_image_file tile_dimensions tilemap_prefix tileset_dimensions')
 
-    cmd = "magick --help > /dev/null"
-    if os.system(cmd) != 0:
-        sys.exit("Install ImageMagick first or put magick in your path")
+    try:
+        check_call(['magick', '-help'], stdout=DEVNULL, stderr=STDOUT)
+    except CalledProcessError:
+        sys.exit('Install ImageMagick or put magick in your path')
 
-    cmd = "identify > /dev/null"
-    if os.system(cmd) != 0:
-        sys.exit("Install ImageMagick first or put identify in your path")
+    try:
+        check_call(['identify', '-help'], stdout=DEVNULL, stderr=STDOUT)
+    except CalledProcessError:
+        sys.exit('Install ImageMagick or put identify in your path')
 
-    if not '/' in sys.argv[3]:
-        sys.exit("You must specify a different output directory")
+    try:
+        check_call(['convert', '-help'], stdout=DEVNULL, stderr=STDOUT)
+    except CalledProcessError:
+        sys.exit('Install ImageMagick or put convert in your path')
 
-    tile_dir = sys.argv[3][0:sys.argv[3].index('/')]
+    if not os.path.exists(sys.argv[1]):
+        sys.exit(f'File {sys.argv[1]} not found')
+    with Image.open(sys.argv[1]) as image:
+        image_w, image_h = image.size
+
+    if not (tile_dim := re.search('(\d+)x(\d+)', sys.argv[2])):
+        sys.exit('Wrong tile dimensions, <number>x<number> expected')
+    else:
+        tile_w, tile_h = int(tile_dim.group(1)), int(tile_dim.group(2))
+    if min(tile_w, tile_h) <= 0:
+        sys.exit('Tile dimensions should be greater than zero')
+
+    if not os.sep in sys.argv[3]:
+        sys.exit('Must specify output directory')
+    else:
+        tile_dir = sys.argv[3][0:sys.argv[3].index(os.sep)]
+        tile_prefix = sys.argv[3][sys.argv[3].index(os.sep) + 1:]
+
     if os.path.exists(tile_dir):
-        sys.exit("Output directory already exists")
+        sys.exit('Output directory already exists')
 
-    print(f"Creating subdirectory {tile_dir}...")
+    if not (tileset_dim := re.search('(\d+)x(\d+)', sys.argv[4])):
+        sys.exit('Wrong tileset dimensions, <number>x<number> expected')
+    else:
+        tileset_w, tileset_h = int(tileset_dim.group(1)), int(tileset_dim.group(2))
+    if min(tileset_w, tileset_h) <= 0:
+        sys.exit('Tileset dimensions should be greater than zero')
+
+    debug(f'Creating subdirectory {tile_dir}...')
     os.mkdir(tile_dir)
 
-    cmd = "magick %s +repage -crop %s %s%%04d.png" % (sys.argv[1], sys.argv[2], sys.argv[3])    
-    ec = os.system(cmd)
+    debug('Cropping tiles from image...')
+    check_call(['magick', sys.argv[1], '+repage', '-crop', sys.argv[2], f'{sys.argv[3]}%04d.png'])
 
-    count = 0
-    chksums = {}
-    for filename in os.listdir(tile_dir):
-        f = os.path.join(tile_dir, filename)
-        chksum = subprocess.check_output(['identify', '-verbose', '-format', '%#', f]).strip()
-        if chksum in chksums:
-            count += 1
-            print(f"Deleting file {f} already accounted for...")
-            cmd = f"echo {f} >> {tile_dir}/removed.txt"
-            os.system(cmd)
-            os.remove(f)
-        else:
-            chksums[chksum] = True
+    deleted = 0
+    chksums = OrderedDict()
+    tiles = []
 
-    print(f"Removed {count} files")
+    with open(os.path.join(tile_dir, 'removed.txt'), 'a+') as output:
+        for filename in sorted(os.listdir(tile_dir)):
+            tile_num = match.group(0) if (match := re.search("[0-9]+", filename)) else None
+            if tile_num is None: continue
+            path = os.path.join(tile_dir, filename)
+            chksum = subprocess.check_output(['identify', '-verbose', '-format', '%#', path]).strip()
+            if chksum in chksums:
+                deleted += 1
+                debug(f'Deleting file {path}: identical tile already accounted for')
+                print(path, file=output)
+                os.remove(path)
+            else:
+                chksums[chksum] = tile_num
+            tiles.append(list(chksums.keys()).index(chksum) + 1)
 
-if __name__ == "__main__":
+    line = 0
+    cmd = ['convert']
+
+    for key, value in chksums.items():
+        if line == 0:
+            cmd.append('(')
+        cmd.append(os.path.join(tile_dir, f'{tile_prefix}{value}.png'))
+        line += 1
+        if line == tileset_w // tile_w:
+            cmd.extend(['+append', ')'])
+            line = 0
+    if line > 0:
+        cmd.extend(['+append', ')'])
+
+    tmp = os.path.join(tile_dir, f'tileset{sys.argv[4]}.png')
+    cmd.extend(['-background', 'black', '-append', tmp])
+    check_call(cmd)
+
+    tiled = {
+        'compression_level': -1,
+        'editorsettings':
+            {
+                'export':
+                    {
+                        'target': '.'
+                    },
+            },
+        'nextlayerid': 2,
+        'nextobjectid': 0,
+        'width': image_w // tile_w,
+        'height': image_h // tile_h,
+        'infinite': False,
+        'tilewidth': tile_w,
+        'tileheight': tile_h,
+        'orientation': 'orthogonal',
+        'renderorder': 'right-down',
+        'tiledversion': '1.3.1',
+        'type': 'map',
+        'version': 1.2,
+        'tilesets':
+            [
+                {
+                    'columns': tileset_w // tile_w,
+                    'firstgid': 1,
+                    'image': f'tileset{sys.argv[4]}.png',
+                    'imagewidth': tileset_w,
+                    'imageheight': tileset_h,
+                    'margin': 0,
+                    'name': tile_prefix,
+                    'spacing': 0,
+                    'tilecount': (image_w // tile_w) * (image_h // tile_h) - deleted,
+                    'tilewidth': tile_w,
+                    'tileheight': tile_h,
+                },
+            ],
+        'layers':
+            [
+                {
+                    'id': 1,
+                    'name': 'Map',
+                    'opacity': 1,
+                    'width': image_w // tile_w,
+                    'height': image_h // tile_h,
+                    'type': 'tilelayer',
+                    'visible': True,
+                    'x': 0,
+                    'y': 0,
+                    'data': tiles,
+                }
+            ],
+    }
+    with open(os.path.join(tile_dir, 'map.json'), 'w') as jsonmap:
+        jsonmap.write(json.dumps(tiled))
+    debug(f'Removed {deleted} files')
+
+if __name__ == '__main__':
     main()
 
